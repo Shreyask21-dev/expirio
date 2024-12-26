c60a753984f28c2f1232f76a34cdb8ddcc8e3b08
vatsalya-sagar <vatsalyasagar8@gmail.com>
2024-12-26 16:45:46 +0530

    Change

--- app/Components/Client.js
+++ app/Components/Client.js
@@ -19,7 +19,7 @@ export default function Client() {
   // Fetch data from the API
   const fetchDomains = async () => {
     try {
-      const response = await axios.get('https://expirio.vercel.app/api/Clients');
+      const response = await axios.get('http://localhost:3000/api/Clients');
       const data = response.data.map((item, index) => ({
         id: index + 1,
         ...item,
@@ -94,7 +94,7 @@ export default function Client() {
        
 
   const deleteRecord = async (id) => {
-    const response = await axios.delete(`https://expirio.vercel.app/api/Clients?id=${id}`)
+    const response = await axios.delete(`http://localhost:3000/api/Clients?id=${id}`)
     if (response.data.message == "Record successfully deleted"){
       alert("Record successfully deleted")
       fetchDomains()
@@ -108,7 +108,7 @@ export default function Client() {
   // Function to handle adding a domain and updating the table data
   const handleAddClients = async (client) => {
     // Update the server with the new domain (same as your DomainForm logic)
-    const response = await axios.post('https://expirio.vercel.app/api/Clients', client);
+    const response = await axios.post('http://localhost:3000/api/Clients', client);
 
     if (response.data.message === 'Domain successfully added') {
       alert('Record added successfully');
@@ -135,7 +135,7 @@ export default function Client() {
 
   const handleUpdate = async (updatedRecord) => {
     try {
-      const response = await axios.put(`https://expirio.vercel.app/api/Clients`, updatedRecord);
+      const response = await axios.put(`http://localhost:3000/api/Clients`, updatedRecord);
       if (response.data.message === 'Record successfully updated') {
         alert('Record updated successfully');
         setPopup(false);
--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -1,5 +1,5 @@
 'use client'
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import Link from "next/link";
 import { useRouter } from 'next/navigation';
@@ -8,18 +8,40 @@ export default function Navbar() {
 
   const router = useRouter()
 
-    useEffect(() => {
-        import('bootstrap/dist/js/bootstrap.js')
-      }, [])
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.js')
+  }, [])
 
   return (
     <div>
-      
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <style>
+        {`
+          .pcView {
+            display: block;
+          }
+
+          .mobileView {
+            display: none !important;
+          }
+
+          @media only screen and (max-width: 600px) {
+            .pcView {
+              display: none !important;
+            }
+
+            .mobileView {
+            display: block !important;
+          }
+          }
+        `}
+      </style>
+
+      {/* pc view */}
+      <nav className="navbar navbar-expand-lg bg-body-tertiary pcView">
         <div className="container">
           <div>
             <Link className="navbar-brand" href="/">
-            <img src='/images/logo.png' style={{width:"20%"}}/>
+              <img src='/images/logo.png' style={{ width: "20%" }} />
             </Link>
           </div>
           <div>
@@ -35,7 +57,47 @@ export default function Navbar() {
                   <Link className="nav-link " aria-current="page" href="/Clients">Clients</Link>
                 </li>
                 <li className='nav-item'>
-                  <button className="btn btn-outline-dark" onClick={()=>{localStorage.setItem('userName','');router.push('/Login/')}}>logout</button>
+                  <button className="btn btn-outline-dark" onClick={() => { localStorage.setItem('userName', ''); router.push('/Login/') }}>logout</button>
+                </li>
+                {/* <li className="nav-item">
+                  <Link className="nav-link " aria-current="page" href="/Subscription">Subscription</Link>
+                </li>  */}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      {/* phone view */}
+      <nav className="navbar navbar-expand-lg bg-body-tertiary mobileView">
+        <div className="container">
+          <div className='d-flex justify-content-between'>
+
+            <div>
+              <Link className="navbar-brand" href="/">
+                <img src='/images/logo.png' style={{ width: "20%" }} />
+              </Link>
+            </div>
+
+            <div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            </div>
+
+          </div>
+          <div>
+            
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link " aria-current="page" href="/">Services</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link " aria-current="page" href="/Clients">Clients</Link>
+                </li>
+                <li className='nav-item'>
+                  <button className="btn btn-outline-dark" onClick={() => { localStorage.setItem('userName', ''); router.push('/Login/') }}>logout</button>
                 </li>
                 {/* <li className="nav-item">
                   <Link className="nav-link " aria-current="page" href="/Subscription">Subscription</Link>
